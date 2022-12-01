c9bc66bd420972c310edc93f1be369dbd7cea040
Stuart Edwards <stuart.edwards@proquoai.com>
2022-12-01 07:13:56 +0200

    2022D1

--- 2022/day_1/solve.py
+++ 2022/day_1/solve.py
@@ -1,2 +1,25 @@
-from utils import common
 
+def solve(path):
+    print(f"--{path}--")
+    cals = []
+    max_cal = 0
+    with open(path) as f:
+        cur_cals = 0
+        for line in f.readlines():
+            if line.strip() == '':
+                cals.append(cur_cals)
+                max_cal = max(max_cal, cur_cals)
+                cur_cals = 0
+            else:
+                cur_cals += int(line.strip())
+
+        cals.append(cur_cals)
+        max_cal = max(max_cal, cur_cals)
+
+    s = sorted(cals)
+    print(f"Max cals: {max_cal}")
+    print(f"Top 3: {sum(s[-3:])}")
+
+
+solve('sample')
+solve('input')
