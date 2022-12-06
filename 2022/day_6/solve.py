acf526dbc74ced3ebf1301d1dd68ad0742a6f754
Stuart Edwards <stuart.edwards@proquoai.com>
2022-12-06 07:09:56 +0200

    Day 6

--- 2022/day_6/solve.py
+++ 2022/day_6/solve.py
@@ -4,12 +4,20 @@ debug = False
 def solve(path):
     print(f"{path:-^20}")
 
-    lines = open(path).readlines()
+    stream = open(path).readlines()[0]
 
-    part1 = None
+    for i in range(len(stream)):
+        if len(set(stream[i:i+4])) == 4:
+            part1 = i+4
+            break
+
     print(f"Part 1: {part1}")
 
-    part2 = None
+    for i in range(len(stream)):
+        if len(set(stream[i:i+14])) == 14:
+            part2 = i+14
+            break
+
     print(f"Part 2: {part2}\n")
 
 
---
+++ 2022/day_7/solve.py
@@ -0,0 +1,17 @@
+debug = False
+
+
+def solve(path):
+    print(f"{path:-^20}")
+
+    lines = open(path).readlines()
+
+    part1 = None
+    print(f"Part 1: {part1}")
+
+    part2 = None
+    print(f"Part 2: {part2}\n")
+
+
+solve('sample')
+solve('input')
