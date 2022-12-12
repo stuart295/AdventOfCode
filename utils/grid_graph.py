49fedcac7722a12d9a309b33b25c819ddcb7c166
Stuart Edwards <stuart.edwards@proquoai.com>
2022-12-12 13:00:49 +0200

    Extended grid graph draw method to draw paths

--- utils/grid_graph.py
+++ utils/grid_graph.py
@@ -17,7 +17,7 @@ class GridGraph:
         to_remove = [node for node, att in self.graph.nodes.items() if att['char'] in walls]
         self.graph.remove_nodes_from(to_remove)
 
-    def draw(self, label_name=None):
+    def draw(self, label_name=None, path=None):
         """
         Draws the current grid graph. If label_name is set, each nodes attribute with this name is used as the label,
         otherwise the coordinates are used
@@ -31,7 +31,14 @@ class GridGraph:
             labels = {node: att[label_name] for node, att in self.graph.nodes.items()}
 
         nx.draw(self.graph, pos=pos, with_labels=True, labels=labels)
+
+        if path:
+            path_edges = [(path[i], path[i+1]) for i in range(len(path)-1)]
+            nx.draw_networkx_edges(self.graph, pos=pos, edgelist=path_edges, edge_color='r', width=5)
+            nx.draw_networkx_nodes(self.graph, pos=pos, nodelist=path, node_color='r')
+
         plt.show()
 
     def shortest_path(self, start, end, weights=None):
         return nx.shortest_path(self.graph, start, end, weight = weights)
+
