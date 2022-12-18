31ef937db220d4b63d10ab1a2cd33f214af3e342
Stuart Edwards <stuart.edwards@proquoai.com>
2022-12-18 09:10:24 +0200

    Minor cleanup

--- 2022/day_17/solve.py
+++ 2022/day_17/solve.py
@@ -9,9 +9,26 @@ rock_types = [
     np.array([[0, 0], [1, 0], [0, 1], [1, 1]]),
 ]
 
+rock_widths = [
+    4,
+    3,
+    3,
+    1,
+    2
+]
 
-def draw(rocks, cur_r, pos, height, width):
+wind_dirs = {
+    '<': np.array([-1, 0]),
+    '>': np.array([1, 0]),
+}
+
+
+def draw(rocks, cur_r, pos, height, width, filename=None):
     print('=' * 20)
+
+    f = None
+    if filename:
+        f = open(filename, 'w')
 
     for h in range(height + 5, -1, -1):
         line = ''
@@ -23,28 +40,44 @@ def draw(rocks, cur_r, pos, height, width):
             else:
                 line += '.'
 
-        print(line)
+        if f:
+            f.write(line + '\n')
+        else:
+            print(line)
+
+    if f:
+        f.close()
 
 
-def simulate(wind, steps=2022, chamber_width=7):
-    rocks = set()
+cache = {}
 
-    rock_idx = 0
-    wind_idx = 0
 
-    highest = 0
+def play_game(wind, start_wind_idx, start_rock_idx, max_steps, chamber_width, start_rocks, h_thresh):
+    key = (wind, start_wind_idx, start_rock_idx, chamber_width, start_rocks, max_steps)
+    if key in cache:
+        return cache[key]
 
-    for step in range(steps):
+    rocks = set(start_rocks)
+
+    rock_idx = start_rock_idx
+    wind_idx = start_wind_idx
+
+    max_heights = [0 for _ in range(chamber_width)]
+
+    if rocks:
+        for rx, ry in rocks:
+            max_heights[rx] = max(max_heights[rx], ry + 1)
+
+    for step in range(max_steps):
         cur_rock = rock_types[rock_idx]
-        pos = np.array([2, highest + 3])
+        rock_idx = (rock_idx + 1) % len(rock_types)
 
+        pos = np.array([2, max(max_heights) + 3])
 
         while True:
-            cur_w = wind[wind_idx]
-            wind_idx = (wind_idx + 1) % len(wind)
-
             # Horizontal movements
-            dir = np.array([-1, 0]) if cur_w == '<' else np.array([1, 0])
+            dir = wind_dirs[wind[wind_idx]]
+            wind_idx = (wind_idx + 1) % len(wind)
 
             new_pos = pos + dir
 
@@ -59,8 +92,6 @@ def simulate(wind, steps=2022, chamber_width=7):
             if not collides:
                 pos = new_pos
 
-
-
             # Vertical
             collides = False
 
@@ -71,36 +102,99 @@ def simulate(wind, steps=2022, chamber_width=7):
                     collides = True
                     break
 
-            if debug: draw(rocks, cur_rock, pos, highest, chamber_width)
+            # debug = True
+            if debug: draw(rocks, cur_rock, pos, max(max_heights), chamber_width)
 
             if collides:
                 for p in cur_rock:
-                    highest = max(highest, (pos + p)[1] +1 )
-                    rocks.add(tuple(pos + p))
+                    pp = pos + p
+                    px, py = pp
+                    rocks.add(tuple(pp))
+
+                    max_heights[px] = max(max_heights[px], py + 1)
+
+                # Epoch check
+                min_h, max_h = min(max_heights), max(max_heights)
+
+                min_p = min(y for x, y in cur_rock + pos)
+                max_p = max(y for x, y in cur_rock + pos)
+
+                can_break = False
+                for ty in range(min_p - 1, max_p + 1):
+                    filled = [False for _ in range(chamber_width)]
+
+                    for x in range(chamber_width):
+                        if (x, ty) in rocks or (x, ty + 1) in rocks:
+                            filled[x] = True
+
+                    if all(filled):
+                        can_break = True
+                        break
+
+                if max_h > h_thresh and can_break:
+                    # draw(rocks, cur_rock, pos, max(max_heights) + 1, chamber_width, filename=str(step))
+                    rem_rocks = []
+                    for x in range(chamber_width):
+                        for y in range(min_p - 1, max_h):
+                            if (x, y) in rocks:
+                                rem_rocks.append((x, y - (min_p - 1)))
+
+                    rem_rocks = sorted(rem_rocks, key=lambda x: x[0] * 1000 + x[1])
+
+                    h_off = max(y for x, y in rem_rocks) + 1
+                    out = max_h - h_off, wind_idx, rock_idx, step, rem_rocks
+                    cache[key] = out
+                    return out
                 break
+
             pos = new_pos
 
+    # draw(rocks, [], [], max(max_heights) + 1, chamber_width, filename=str(max_steps))
+    return max(max_heights), wind_idx, rock_idx, max_steps, []
 
 
+def simulate(wind, steps=2022, chamber_width=7):
+    rock_idx = 0
+    wind_idx = 0
 
-        rock_idx = (rock_idx + 1) % len(rock_types)
+    highest = 0
 
-    return rocks, highest
+    rem_steps = steps
+    wind_tup = tuple(wind)
+
+    start_rocks = []
+
+    while rem_steps > 0:
+        print(f"Remaining steps: {rem_steps}")
+
+        h_thresh = 500000
+
+        max_steps = 5000000
+        if rem_steps < max_steps:
+            max_steps = min(rem_steps, 10000)
+            h_thresh = 1000
+
+        h, wind_idx, rock_idx, s, start_rocks = play_game(wind_tup, wind_idx, rock_idx, max_steps,
+                                                          chamber_width, tuple(start_rocks), h_thresh)
+
+        highest += h
+        rem_steps -= s + 1
+
+    return highest
 
 
 def solve(lines):
     wind = list(lines[0].strip())
 
-    rocks, highest = simulate(wind)
+    highest = simulate(wind)
 
     part1 = highest
 
-    rocks, highest = simulate(wind, steps=1000000000000)
+    highest = simulate(wind, steps=1000000000000)
     part2 = highest
-
     return part1, part2
 
 
 debug = False
-solve_puzzle(year=2022, day=17, solver=solve, do_sample=True, do_main=False, sample_data_path='sample')
-# solve_puzzle(year=2022, day=17, solver=solve, do_sample=False, do_main=True)
+# solve_puzzle(year=2022, day=17, solver=solve, do_sample=True, do_main=False, sample_data_path='sample')
+solve_puzzle(year=2022, day=17, solver=solve, do_sample=False, do_main=True)
