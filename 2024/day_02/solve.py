24266f84af4637037a83f2ace9c6d5e3f968b78c
Stuart Edwards <stuart.edwards@versityacademy.ai>
2024-12-02 10:04:19 +0200

    Day 2 - Cleaned up

--- 2024/day_02/solve.py
+++ 2024/day_02/solve.py
@@ -1,5 +1,4 @@
 from utils.common import solve_puzzle
-from collections import Counter
 
 YEAR = 2024
 DAY = 2
@@ -9,8 +8,7 @@ def solve(lines):
     lines = [[int(l) for l in line.split(" ")] for line in lines]
 
     part1 = solve_part_01(lines)
-
-    part2 = solve_part_02(lines)
+    part2 = solve_part_01(lines, use_damping=True)
 
     return part1, part2
 
@@ -25,19 +23,12 @@ def is_line_safe(line) -> bool:
     return min(diffs) >= 1 and max(diffs) <= 3
 
 
-def solve_part_01(lines):
-    result = 0
-    for line in lines:
-        result += is_line_safe(line)
-    return result
-
-
-def solve_part_02(lines):
+def solve_part_01(lines, use_damping=False):
     result = 0
     for line in lines:
         if is_line_safe(line):
             result += 1
-        else:
+        elif use_damping:
             # Glorious brute forcing
             for i in range(len(line)):
                 new_line = line[0:i] + line[i + 1:]
@@ -46,6 +37,6 @@ def solve_part_02(lines):
                     break
     return result
 
-debug = True
+
 solve_puzzle(year=YEAR, day=DAY, solver=solve, do_sample=True, do_main=False)
 solve_puzzle(year=YEAR, day=DAY, solver=solve, do_sample=False, do_main=True)
