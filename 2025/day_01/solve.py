94105a3e665bf85b5cf3ae704aafd000090813ec
Stuart Edwards <stuart.edwards@versityacademy.ai>
2025-12-01 11:07:32 +0200

    2025 day 1 - Cleanup

--- 2025/day_01/solve.py
+++ 2025/day_01/solve.py
@@ -4,7 +4,6 @@ YEAR = 2025
 DAY = 1
 
 def solve(lines):
-
     pos = 50
     part1 = 0
     part2 = 0
@@ -14,21 +13,24 @@ def solve(lines):
         if direction == 'L':
             dir_const = -1
 
-        if dist > 0:
-            remaining_distance = dist
-            if pos != 0:
-                if direction == 'L':
-                    remaining_distance = dist - pos
-                elif direction == 'R':
-                    remaining_distance = dist - (100 - pos)
+        # Part 2
+        remaining_distance = dist
 
-                if remaining_distance >= 0:
-                    part2 += 1
+        # Move towards origin
+        if pos != 0:
+            if direction == 'L':
+                remaining_distance = dist - pos
+            elif direction == 'R':
+                remaining_distance = dist - (100 - pos)
 
-            if remaining_distance > 0:
-                part2 += remaining_distance // 100
+            if remaining_distance >= 0:
+                part2 += 1
 
+        # Calculate additional loops
+        if remaining_distance > 0:
+            part2 += remaining_distance // 100
 
+        # Part 1
         pos = (pos + dir_const * dist) % 100
         if pos == 0:
             part1 += 1
@@ -36,21 +38,5 @@ def solve(lines):
     return part1, part2
 
 
-with open("test.txt", 'r') as f:
-    test_lines = f.readlines()
-
-with open("input.txt", 'r') as f:
-    lines = f.readlines()
-
-p1, p2 = solve(test_lines)
-print(f"Part 1: {p1}")
-print(f"Part 2: {p2}")
-
-p1, p2 = solve(lines)
-
-print(f"Part 1: {p1}")
-print(f"Part 2: {p2}")
-
-
-# solve_puzzle(year=YEAR, day=DAY, solver=solve, do_sample=True, do_main=False)
-# solve_puzzle(year=YEAR, day=DAY, solver=solve, do_sample=False, do_main=True)
+solve_puzzle(year=YEAR, day=DAY, solver=solve, do_sample=True, do_main=False)
+solve_puzzle(year=YEAR, day=DAY, solver=solve, do_sample=False, do_main=True)
