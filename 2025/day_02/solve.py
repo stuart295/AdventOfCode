01cd169eaf12b896c828414596eab4031ee5846c
Stuart Edwards <stuart.edwards@versityacademy.ai>
2025-12-02 17:51:48 +0200

    2025 day 2 - Cleanup

--- 2025/day_02/solve.py
+++ 2025/day_02/solve.py
@@ -3,6 +3,7 @@ from utils.common import solve_puzzle
 YEAR = 2025
 DAY = 2
 
+
 def solve(lines):
     if len(lines) > 1:
         raw = ''.join(lines)
@@ -12,58 +13,15 @@ def solve(lines):
     ranges = [range.split("-") for range in ranges]
 
     part1 = solve_part_01(ranges)
-
     part2 = solve_part_02(ranges)
 
     return part1, part2
 
 
-def solve_part_01(ranges: list[str]) -> int:
-    part1 = 0
-
-    for first, last in ranges:
-        first_int = int(first)
-        last_int = int(last)
-
-        cur_num = first
-
-        if len(cur_num) % 2 == 0:
-            first_half = cur_num[:len(cur_num) // 2]
-            first_half_int = int(first_half)
-            cur_num = str(first_half_int) * 2
-            if first_int <= int(cur_num) <= last_int:
-                part1 += int(cur_num)
-
-        while int(cur_num) <= last_int:
-            if len(cur_num) % 2 != 0:
-                cur_num = "1" + "0" * len(cur_num)
-                cur_num = cur_num[:len(cur_num) // 2] * 2
-                cur_num_int = int(cur_num)
-                if first_int <= cur_num_int <= last_int:
-                    part1 += cur_num_int
-                continue
-
-            first_half = cur_num[:len(cur_num) // 2]
-            first_half_int = int(first_half) + 1
-            cur_num = str(first_half_int) * 2
-
-            cur_num_int = int(cur_num)
-            if first_int <= cur_num_int <= last_int:
-                part1 += cur_num_int
-    return part1
-
 def check_invalids(first, last, splits, invalid_ids):
     first_int = int(first)
     last_int = int(last)
     cur_num = first
-
-    if len(cur_num) % splits == 0 and splits <= len(cur_num):
-        first_half = cur_num[:len(cur_num) // splits]
-        first_half_int = int(first_half)
-        cur_num = str(first_half_int) * splits
-        if first_int <= int(cur_num) <= last_int and cur_num not in invalid_ids:
-            invalid_ids.add(int(cur_num))
-            # print(cur_num)
 
     while int(cur_num) <= last_int:
         if len(cur_num) % splits != 0:
@@ -71,35 +29,36 @@ def check_invalids(first, last, splits, invalid_ids):
             cur_num = cur_num[:len(cur_num) // 2] * splits
             cur_num_int = int(cur_num)
             if first_int <= cur_num_int <= last_int:
-                invalid_ids.add(int(cur_num))
-            continue
-
-        if splits > len(cur_num):
-            cur_num = "1" + "0" * len(cur_num)
-            cur_num = cur_num[:len(cur_num) // 2] * splits
-            cur_num_int = int(cur_num)
-            if first_int <= cur_num_int <= last_int:
-                invalid_ids.add(int(cur_num))
-                # print(cur_num)
+                invalid_ids.add(cur_num_int)
             continue
 
         first_half = cur_num[:len(cur_num) // splits]
-        first_half_int = int(first_half) + 1
+        first_half_int = int(first_half)
         cur_num = str(first_half_int) * splits
 
         cur_num_int = int(cur_num)
         if first_int <= cur_num_int <= last_int:
-            invalid_ids.add(int(cur_num))
-            # print(cur_num)
+            invalid_ids.add(cur_num_int)
+
+        first_half_int += 1
+        cur_num = str(first_half_int)*splits
+
+
+def solve_part_01(ranges: list[str]) -> int:
+    invalid_ids = set()
+    for first, last in ranges:
+        check_invalids(first, last, 2, invalid_ids)
+
+    return sum(invalid_ids)
 
 
 def solve_part_02(ranges: list[str]) -> int:
     invalid_ids = set()
 
     for first, last in ranges:
-        for splits in range(2, len(last)+1):
+        for splits in range(2, len(last) + 1):
             check_invalids(first, last, splits, invalid_ids)
-    # print(sorted(list(invalid_ids)))
+
     return sum(invalid_ids)
 
 
